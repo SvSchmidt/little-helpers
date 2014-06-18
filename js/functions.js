f2ff87d9089e6fd7fef9908cef40f3082c17d371
SvSchmidt <SvSchmidt@users.noreply.github.com>
2014-06-18 15:42:31 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,28 @@
-if(typeof console === "undefined") {
-	console = {
-		log:function() {},
-		debug:function() {},
-		error:function() {}
-	};
+function checkConsole() {
+	if(typeof console === "undefined") {
+		console = {
+			log:function() {},
+			debug:function() {},
+			error:function() {},
+			debug:function() {}
+		};
+	}
+}
+
+Object.prototype.toArray = function(arrayVal) {
+	return [obj].map(function(index) {
+		return index[arrayVal];
+	});
+}
+/*var obj = {name:"sven"};
+obj.toArray("name")*/
+
+/**
+ * does exactly what function name suggests
+ * Usage: "string".capitaliseFirstLetter()
+ * @param  {String} str
+ * @return {String}     str with capitalised first letter (e.g. Test if str equals test)
+ */
+String.prototype.capitaliseFirstLetter = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1);
 }
