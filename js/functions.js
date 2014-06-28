eb28bec90c80eef5b8494efdb6a8e5fd9769c1ba
SvSchmidt <SvSchmidt@users.noreply.github.com>
2014-06-28 19:16:34 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -18,15 +18,15 @@ function checkConsole() {
 /**
  * does exactly what function name suggests
  * @param  {String} str
- * @return {String}     str with capitalised first letter (e.g. Test if str equals test)
+ * @return {String}     str with capitalized first letter (e.g. Test if str equals test)
  */
-String.prototype.capitaliseFirstLetter = function() {
+String.prototype.capitalizeFirstLetter = function() {
 	return this.charAt(0).toUpperCase() + this.slice(1);
 }
 /*"test".capitaliseFirstLetter()*/
 
-QUnit.test("str.capitaliseFirstLetter()",function() {
-	equal("test".capitaliseFirstLetter(),"Test","convert test into Test");
+QUnit.test("str.capitalizeFirstLetter()",function() {
+	equal("test".capitalizeFirstLetter(),"Test","convert test into Test");
 });
 
 /*------------------------------------*/
@@ -72,4 +72,58 @@ QUnit.test("Object.prototype.hasClasses(classList)",function() {
 	ok(document.documentElement.hasClasses(["js","root-section"]),"handle class list as array (e.g. ['class1','class2'])");
 	ok(!document.documentElement.hasClasses("js root-section bullshit"),"return false if one class of classList is missing in element (all-or-none)")
 	ok(!document.documentElement.hasClasses("stupid classes"),"return false if element does not have specified class(es)")
+});
+
+/*------------------------------------*/
+
+/**
+ * checks if the used browser knows the specified CSS attribute (e.g. filter)
+ * @return {boolean} true if browser knows specified attribute, false if not
+ */
+navigator.supports = function(attribute) {
+	var vendorPrefixes = ["Moz","Webkit","Khtml","O","ms",""];
+	for(var i = 0; i < vendorPrefixes.length; i++) {
+		if(typeof((document.body || document.documentElement).style[vendorPrefixes[i] + attribute]) == "string") return true;
+	}
+
+	return false;
+}
+
+QUnit.test("navigator.supports(attribute)",function() {
+	ok(navigator.supports("color"),"return true if used browser knows specified attribute respecting vendorprefixes");
+	ok(!navigator.supports("bullshit"),"return false if specified attribute respecting vendorprefixes is unknown to browser");
+});
+
+/*------------------------------------*/
+
+/**
+ * returns either vendorprefix of current browser or vendor-prefixed @attribute
+ * even if there isn't any prefixed value set, getComputedStyle will return the initial values
+ * @param  {String} attribute e.g. transform
+ * @return {String} vendor-prefix (e.g. Webkit) or vendor-prefixed attribute (e.g. WebkitTransform)
+ */
+function vendorPrefix(attribute) {
+	var vendorPrefix = (Array.prototype.slice.call(window.getComputedStyle(document.documentElement,""))).join("").match(/-(moz|webkit|ms)-/);
+	var objResult = {
+		"prefix": {
+			"css":vendorPrefix[0],
+			"js":vendorPrefix[1].capitalizeFirstLetter(),
+		},
+		"attribute": {
+			"css":vendorPrefix[0] + attribute,
+			"js":vendorPrefix[1].capitalizeFirstLetter() + (attribute ? attribute.capitalizeFirstLetter() : "")
+		}
+	}
+
+	if(typeof window.getComputedStyle(document.documentElement)[attribute] === "string") return attribute;
+	if(typeof window.getComputedStyle(document.documentElement)[objResult.attribute.css] === "string") return objResult.attribute.js;
+
+	return objResult.prefix.js;
+}
+
+QUnit.test("vendorPrefix(attribute)",function() {
+	equal(vendorPrefix(),"Webkit","return vendorprefix (WebKit) if no attribute's provided");
+	equal(vendorPrefix("ffjfjaf"),"Webkit","do so too if prefixed attribute is unknown to browser");
+	equal(vendorPrefix("transform"),"WebkitTransform","prefix the attribute if provided");
+	equal(vendorPrefix("transition"),"transition","do not prefix the attribute if the browser knows both the variant with and without prefix (e.g. transition)");
 });
