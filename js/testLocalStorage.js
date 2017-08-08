ac9f5b0200f7a7ade7fc3a05876f6b4761a91f98
Sven Schmidt <SvSchmidt@users.noreply.github.com>
2017-08-08 09:32:15 +0200

    Update testLocalStorage.js

--- js/testLocalStorage.js
+++ js/testLocalStorage.js
@@ -1,49 +1,83 @@
-var getLocalStorageLimit = function() {
-    return interpolateLimit();
+console.table = console.table || console.info;
+
+function getStorageLimit (storage) {
+  const rememberedData = {};
+
+  // Remember stored data
+  Object.keys(storage).forEach(k => {
+    rememberedData[k] = storage[k];
+  });
+
+  storage.clear();
+
+  /* The idea is the following:
+   * try to store a very long string (10000000 bytes), and catch errors,
+   * then try again with 10000000 / 10 etc. until no error is thrown
+   */
+  function interpolateLimit (step, startAt, endAt) {
+      startAt = startAt || 0;
+      step = step || 100000;
+      endAt = endAt || 10000000;
+
+      let max = 10000000;
+
+      storage.clear();
+
+      for (let i = startAt; i < endAt; i += step) {
+          try {
+              storage.setItem('a', 'a'.repeat(i));
+          } catch () {
+              if (step <= 1) {
+                  max = i;
+              } else {
+                  max = interpolateLimit(step / 10, i - step, i);
+              }
+              
+              break;
+          }
+      }
+      
+      return max;
+  }
+
+  const result = interpolateLimit();
+
+  storage.clear();
+
+  // Restore data to storage
+  Object.keys(rememberedData).forEach(k => {
+    storage[k] = rememberedData[k];
+    delete rememberedData[k];
+  });
+  
+  return result;
 }
 
-var interpolateLimit = function(step, startAt, endAt) {
-    startAt = startAt || 0;
-    step = step || 100000;
-    endAt = endAt || 10000000;
-
-    var max = 10000000;
-
-    localStorage.clear();
-
-    for (var i = startAt; i < endAt; i += step) {
-        try {
-            localStorage.setItem('test', 'a'.repeat(i));
-        } catch(e) {
-            if(step <= 1) {
-                max = i;
-            } else {
-                max = interpolateLimit(step / 10, i - step, i);
-            }
-            break;
-        }
-    }
-
-    localStorage.clear();
-    return max;
-}
-
-var getLocalStorageUsage = function() {
-    var total = 0;
-    for(var x in localStorage) {
-        var amount = (localStorage[x].length * 2);
+function getStorageUsage (storage) {
+    let total = 0;
+    let table = [];
+    
+    Object.keys(storage).forEach(k => {
+        const amount = localStorage[k].length + k.length;
+        
         total += amount;
-        console.log( x + " = " + (amount / 1024 / 1024).toFixed(10));
-    }
+        table.push({ k, amount: MiB(amount, 10) });
+    });
+
+    console.table(table);
     
     return total;
 }
 
-var testLocalStorage = function() {
-    var usage = getLocalStorageUsage();
-    var capacity = getLocalStorageLimit();
+function testStorage (storage) {
+    const usage = getStorageUsage(storage);
+    const capacity = getStorageLimit(storage);
 
-    console.info('localStorage Capacity: ' + (capacity / 1024 / 1024).toFixed(10) + 'MB');
-    console.info('localStorage Usage: ' + (usage / 1024 / 1024).toFixed(10) + 'MB');
-    console.info('localStorage remaining: ' + ((capacity - usage) / 1024 / 1024).toFixed(10) + 'MB');
+    console.info('Capacity: ' + MiB(capacity) + ' MiB');
+    console.info('Usage: ' + MiB(usage) + ' MiB');
+    console.info('Remaining: ' + MiB(capacity - usage) + ' MiB');
+}
+
+function MiB (bytes, dec = 2) {
+  return (bytes / 1024 / 1024).toFixed(dec);
 }
